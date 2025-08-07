e2382056d211af763ffe8f4493e0f9c5b7325ea6
akshay.t <akshay.t@softobotics.com>
2025-08-08 04:16:22 +0530

    fixed the build issue

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,14 +2,14 @@
   window["env"] = window["env"] || {};
 
   // BackEnd Environment variables
-  window["env"]["fineractApiUrls"] = '';
-  window["env"]["fineractApiUrl"]  = '';
+  window["env"]["fineractApiUrls"] = 'https://core.betterfinco.com';
+  window["env"]["fineractApiUrl"]  = 'https://core.betterfinco.com';
 
   window["env"]["apiProvider"] = '';
   window["env"]["apiVersion"]  = '';
 
-  window["env"]["fineractPlatformTenantId"]  = '';
-  window["env"]["fineractPlatformTenantIds"]  = '';
+  window["env"]["fineractPlatformTenantId"]  = 'default';
+  window["env"]["fineractPlatformTenantIds"]  = 'default';
 
   // Language Environment variables
   window["env"]["defaultLanguage"] = '';
