c2d0150ea13b59a2cd765d342d1eb7ece48e6439
akshay.t <akshay.t@softobotics.com>
2025-09-29 12:16:48 +0530

    base url changes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -20,8 +20,7 @@ export const environment = {
   baseApiUrls: 'https://core-dev.betterfinco.com',
   // For connecting to server running elsewhere set the base API URL
   baseApiUrl:
-    loadedEnv['fineractApiUrl'] ||
-    (loadedEnv['fineractApiUrls']?.length > 0 ? loadedEnv['fineractApiUrls'].split(',')[0] : window.location.origin),
+    'https://core-dev.betterfinco.com',
   allowServerSwitch: env.allow_switching_backend_instance,
   apiProvider: loadedEnv['apiProvider'] || '/fineract-provider/api',
   apiVersion: loadedEnv['apiVersion'] || '/v1',
